e2877e1bfbef035b4ed1b3ee97a703172eeb7aaf
Muhammadkaj11 <muhdkaj11@gmail.com>
2024-11-08 21:12:39 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+def weather_condition():
+    print("We are in autumn season")
+    print("We are in spring and the flowers are bloming")
+weather_condition()
---
+++ python.py
@@ -0,0 +1,13 @@
+def add(a,c):
+    return a+c
+def sub(a,c):
+    return a-c
+def div(a,c):
+    return a//c
+def mult (a,c):
+    return a*c
+
+print(add(50,20))
+print(sub(50,20))
+print(div(50,20))
+print(mult(50,20))
